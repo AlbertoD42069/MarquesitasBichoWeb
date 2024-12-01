43801723042309e327ef70ef90a9b507377d5d87
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-11-30 22:10:34 -0600

    Agregado de firebase al proyecto

---
+++ src/FireBaseConfig/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCb1aer4-e21GITY_Xa5b1s7fyxbzKU4fw",
+  authDomain: "marquesitasbicho-7e2ab.firebaseapp.com",
+  databaseURL: "https://marquesitasbicho-7e2ab-default-rtdb.firebaseio.com",
+  projectId: "marquesitasbicho-7e2ab",
+  storageBucket: "marquesitasbicho-7e2ab.firebasestorage.app",
+  messagingSenderId: "345260775018",
+  appId: "1:345260775018:web:b14e69039bdf1c270a9c13",
+  measurementId: "G-4FJN2SGQ6T"
+};
+export const appMaqBicho = initializeApp(firebaseConfig);
