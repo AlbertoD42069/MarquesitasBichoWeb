bd1f8fe323f6542c3612a7750c8c157b6c636644
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-11-30 21:32:32 -0600

    Se inicializaron los componentes y se agrego boostrap como libreria

---
+++ src/Components/HomeBicho.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBarBicho from './SideBarBicho'
+import MenuBicho from './MenuBicho'
+import SuggestionsBicho from './SuggestionsBicho'
+import TransferData from './TransferData'
+
+
+export default function HomeBicho() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/Components/MenuBicho.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MenuBicho() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
