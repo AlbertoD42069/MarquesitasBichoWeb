9673638b67fe8d35ff88aca627a42283c6949514
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-11-30 20:12:01 -0600

    Creacion de componentes

---
+++ src/Components/StaticTexts.js
@@ -0,0 +1,27 @@
+
+export const StaticTexts = {
+    ingredients : 'Ingredientes',
+    suggestions :  'Sugerencias',
+    menu : 'Menu',
+    prices : 'Precios',
+    dataTranfer: 'Datos de tranferencia',
+    nameOfOwner: 'Titular', //nombre del titular de la cuenta bancaria
+    bankKeyAccount: 'Cuenta clabe',
+    bank: 'Banco',
+    phoneNumber: 'Numero de telefono',
+    type: 'tipo',
+    locationTitle: 'Encuentranos en.',
+    locationBody: 'Periferico Carlos Pellicer Cámara 134, Reforma, 86090 Villahermosa, Tabasco',
+    oneIngredient: 'Un ingrediente',
+    twoIngredient: 'Dos ingredientes',
+    three: 'Tres Ingredientes,',
+    bombom: ' Bombom',
+    nut: 'Nuez',
+    banana: 'Platanos',
+    kiwi: 'Kiwi',
+    footerPhrase: 'Frase pie de pagina',
+    holder: 'luis antonio tovilla suarez',
+    accountNumberKey : '012 790 015 41086621 1',
+    bank: 'BBVA',
+    phoneNumberData: "993 118 0228",
+}
