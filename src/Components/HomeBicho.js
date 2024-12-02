95d39c3a2341986b5af6dec2871c57079e61e587
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-12-01 19:32:14 -0600

    agregado de navegacion del navbar ya funciona

--- src/Components/HomeBicho.js
+++ src/Components/HomeBicho.js
@@ -4,15 +4,18 @@ import SuggestionsBicho from './SuggestionsBicho.js';
 import TransferData from './TransferData'
 import Container from 'react-bootstrap/Container';
 import MenuBicho from './MenuBicho.js';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 export default function HomeBicho() {
   return (
     <div>
-      <NavBarBicho/>
-      <SuggestionsBicho/>
-      <MenuBicho/>
-      <TransferData/>
+       <NavBarBicho/>
+      <Routes>
+        <Route path='/' element={<SuggestionsBicho/>}/>
+        <Route path='menu' element={<MenuBicho/>}/>
+        <Route path='datosTransferencia' element={<TransferData/>}/>
+      </Routes>
     </div>
   )
 }
--- src/Components/NavBarBicho.js
+++ src/Components/NavBarBicho.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { TextStatics } from '../Resources/TextStatics'
+import { Link } from "react-router-dom";
+
 
 
 export default function NavBarBicho() {
@@ -12,11 +13,11 @@ export default function NavBarBicho() {
     <>
     <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">{TextStatics.title}</Navbar.Brand>
+          <Navbar.Brand>{TextStatics.title}</Navbar.Brand>
           <Nav className="justify-content-end">
-            <Nav.Link href="#home">{TextStatics.suggestions}</Nav.Link>
-            <Nav.Link href="#features">{TextStatics.menu}</Nav.Link>
-            <Nav.Link href="#pricing">{TextStatics.transferData}</Nav.Link>
+            <Nav.Link href='/'>{TextStatics.suggestions}</Nav.Link>
+            <Nav.Link href='menu'>{TextStatics.menu}</Nav.Link>
+            <Nav.Link href='datosTransferencia'>{TextStatics.transferData}</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
